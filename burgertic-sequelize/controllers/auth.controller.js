94d5c62250e58b8c8d1f95ef9f9926b22e38c459
PICCAGLI BENJAMIN <48386751@ort>
2024-11-16 19:39:13 -0300

    Termine login y avance jwt

--- burgertic-sequelize/controllers/auth.controller.js
+++ burgertic-sequelize/controllers/auth.controller.js
@@ -36,12 +36,12 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
     const { email, password } = req.body;
-
+console.log(email)
     if (!email || !password)
         return res.status(400).json({ message: "Faltan campos por llenar" });
 
     const usuario = await UsuariosService.getUsuarioByEmail(email);
-
+console.log(usuario)
     if (!usuario)
         return res.status(400).json({ message: "Usuario no encontrado" });
 
--- burgertic-sequelize/services/usuarios.service.js
+++ burgertic-sequelize/services/usuarios.service.js
@@ -1,3 +1,4 @@
+import { raw } from "express";
 import { Usuario } from "../models/usuarios.model.js";
 
 const getAllUsuarios = async () => await Usuario.findAll();
@@ -21,13 +22,25 @@ const getAllUsuarios = async () => await Usuario.findAll();
     }
 };
  */
-    const getUsuarioByEmail = async (email)  =>{
-       console.log(email)
-        await Usuario.findAll({
-            where:{
-                email:email,
+const getUsuarioByEmail = async (email) => {
+    try {
+        console.log("Email recibido en el servicio:", email);
+        
+        const usuario = await Usuario.findOne({
+            where: {
+                email: email,
             },
-        });}
+            raw: true,
+        });
+
+        console.log("Usuario encontrado:", usuario);
+        return usuario;
+    } catch (error) {
+        console.error("Error al buscar usuario por email:", error.message);
+        throw new Error("Error al buscar usuario");
+    }
+};
+
 
 /* const getUsuarioById = async (id) => {
     const client = new Client(config);
@@ -48,12 +61,24 @@ const getAllUsuarios = async () => await Usuario.findAll();
     }
 };
  */
-const getUsuarioById = async (id) =>
-await Usuario.findAll({
-    where:{
-        id:id,
-    },
-});
+const getUsuarioById = async (id) => {
+    try {
+        console.log("Email recibido en el servicio:", id);
+        
+        const usuario = await Usuario.findOne({
+            where: {
+                id: id,
+            },
+            raw: true,
+        });
+
+        console.log("Usuario encontrado:", usuario);
+        return usuario;
+    } catch (error) {
+        console.error("Error al buscar usuario por email:", error.message);
+        throw new Error("Error al buscar usuario");
+    }
+};
 
 /* const createUsuario = async (usuario) => {
     const client = new Client(config);
