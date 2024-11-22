19fe285088d7d7582238ba99bd30a85066c229e9
UriZaltzman <144272394+UriZaltzman@users.noreply.github.com>
2024-11-22 09:02:33 -0300

    modifico los service y paso de sql a sequelize

--- burgertic-sequelize/services/platos.service.js
+++ burgertic-sequelize/services/platos.service.js
@@ -1,45 +1,36 @@
-import { Plato } from "../models/platos.model.js";
+import { Plato } from '../models/platos.model.js';
 
-const getPlatos = async () => await Plato.findAll();
+const getPlatos = async () => {
+    return await Plato.findAll();
+};
 
-const getPlatoById = async (id) =>
-    await Plato.findAll({
-        where: {
-            id: id,
-        },
-    });
+const getPlatoById = async (id) => {
+    return await Plato.findByPk(id);
+};
 
-const createPlato = async (plato) =>
-    Plato.create({
-        tipo: plato.tipo,
-        nombre: plato.nombre,
-        precio: plato.precio,
-        descripcion: plato.descripcion,
-    });
+const createPlato = async (plato) => {
+    return await Plato.create(plato);
+};
 
 const updatePlato = async (id, newData) => {
     const plato = await Plato.findByPk(id);
-
     if (!plato) throw new Error("Plato no encontrado");
-
-    plato.tipo = newData.tipo;
-    plato.nombre = newData.nombre;
-    plato.precio = newData.precio;
-    plato.descripcion = newData.descripcion;
-
-    await plato.save();
+    
+    await plato.update(newData);
+    return plato;
 };
 
 const deletePlato = async (id) => {
     const plato = await Plato.findByPk(id);
-
     if (!plato) throw new Error("Plato no encontrado");
-
+    
     await plato.destroy();
+    return plato;
 };
 
-const getPlatosByTipo = async (tipo) =>
-    Plato.findAll({ where: { tipo: tipo } });
+const getPlatosByTipo = async (tipo) => {
+    return await Plato.findAll({ where: { tipo } });
+};
 
 export default {
     getPlatos,
--- burgertic-sequelize/services/usuarios.service.js
+++ burgertic-sequelize/services/usuarios.service.js
@@ -1,61 +1,19 @@
-import { config } from "../db.js";
-import pkg from "pg";
-const { Client } = pkg;
+import { Usuarios } from '../models/usuarios.model.js';
 
 const getUsuarioByEmail = async (email) => {
-    const client = new Client(config);
-    await client.connect();
-
-    try {
-        const { rows } = await client.query(
-            "SELECT * FROM usuarios WHERE email = $1",
-            [email]
-        );
-        if (rows.length < 1) return null;
-
-        await client.end();
-        return rows[0];
-    } catch (error) {
-        await client.end();
-        throw error;
-    }
+    return await Usuarios.findOne({ where: { email } });
 };
 
 const getUsuarioById = async (id) => {
-    const client = new Client(config);
-    await client.connect();
-
-    try {
-        const { rows } = await client.query(
-            "SELECT * FROM usuarios WHERE id = $1",
-            [id]
-        );
-        if (rows.length < 1) return null;
-
-        await client.end();
-        return rows[0];
-    } catch (error) {
-        await client.end();
-        throw error;
-    }
+    return await Usuarios.findByPk(id);
 };
 
 const createUsuario = async (usuario) => {
-    const client = new Client(config);
-    await client.connect();
-
-    try {
-        const { rows } = await client.query(
-            "INSERT INTO usuarios (nombre, apellido, email, password, admin) VALUES ($1, $2, $3, $4, false)",
-            [usuario.nombre, usuario.apellido, usuario.email, usuario.password]
-        );
-
-        await client.end();
-        return rows;
-    } catch (error) {
-        await client.end();
-        throw error;
-    }
+    return await Usuarios.create(usuario);
 };
 
-export default { getUsuarioByEmail, getUsuarioById, createUsuario };
+export default {
+    getUsuarioByEmail,
+    getUsuarioById,
+    createUsuario,
+};
